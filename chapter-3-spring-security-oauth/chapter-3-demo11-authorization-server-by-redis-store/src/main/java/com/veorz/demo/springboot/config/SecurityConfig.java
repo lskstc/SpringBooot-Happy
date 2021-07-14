18b225c6ff75f4ee487d1e984ff58725f045cf0c
lskstc <lskstc@gmail.com>
2021-07-14 15:49:41 +0800

    添加chapter3的密码模式-redis存储器示例：

---
+++ chapter-3-spring-security-oauth/chapter-3-demo11-authorization-server-by-redis-store/src/main/java/com/veorz/demo/springboot/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.veorz.demo.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+/**
+ * @program: SpringBoot-Learning
+ * @description: oauth配置类
+ * @author: lsk
+ * @create: 2021/06/28
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder(){
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.
+                // 使用内存中的 InMemoryUserDetailsManager
+                inMemoryAuthentication()
+                // 不使用 PasswordEncoder 密码编码器
+                .passwordEncoder(passwordEncoder())
+                // 配置 caicat 用户
+                .withUser("caicat").password("1024").roles("USER");
+    }
+}
